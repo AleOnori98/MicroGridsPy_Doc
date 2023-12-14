78cb9796e0b5c883ca6294c11af28887a5f7c2d1
AleOnori98 <onorialessandro1998@gmail.com>
2023-12-14 16:37:33 +0100

    new conf

---
+++ docs/source/conf_standard.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'MicroGridsPy'
+copyright = '2021, SESAM Polimi'
+author = 'SESAM Polimi'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.images',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# Add custom CSS
+html_css_files = [
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+    'custom.css',
+]
+
+# Add custom JavaScript
+html_js_files = [
+    'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
+]
+
