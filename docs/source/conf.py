4c3acb8fd06b885caacbdae69274f821a136db45
AleOnori98 <onorialessandro1998@gmail.com>
2023-12-15 10:28:40 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,16 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinxcontrib.images'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+    "nbsphinx_link",
+    "sphinx.ext.imgconverter",
+    "sphinxcontrib.images"
 ]
 
 intersphinx_mapping = {
@@ -30,7 +34,13 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+
+html_theme_options = {
+    "repository_url": "https://github.com/SESAM-Polimi/MicroGridsPy-SESAM",
+    "use_repository_button": True,
+    "show_navbar_depth": 1,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
@@ -47,4 +57,5 @@ html_js_files = [
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
 ]
+
 
